8de1fa7b33b08d00ff1f6e6034f1af39c21c904c
shshamim63 <shshamim63@gmail.com>
2025-01-15 22:23:20 +0600

    Introduced react router and styled component setup

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,22 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route index element={<Navigate replace to="dashboard" />} />
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path element={<Login />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route element={
+          
+        }/>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+    // <Routes>
+    //   <Route index element={<Navigate replace to="dashboard" />} />
+    //   <Route path="dashboard" element={<Dashboard />} />
+    //   <Route path element={<Login />} />
+    // </Routes>
   );
 };
 
